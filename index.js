3328078df2d45b45a7f1819f6222b9eb83fc358c
Elene Tabidze <etabidze@outlook.com>
2021-07-18 20:28:55 +0400

    back trakuesh

--- index.js
+++ index.js
@@ -14,36 +14,36 @@ const CronJob = require("cron").CronJob;
 const { Jewel } = require("./models/jewel");
 const passwordReset = require("./routes/passwordReset");
 
-require('dotenv').config();
+require("dotenv").config();
 
-// const expChecker = async () => {
-//   const jewelsArray = await Jewel.find();
-//   let jewelsArrayFiltered = jewelsArray.filter((item) => {
-//     return item.type.name === "Super" || "VIP";
-//   });
-//   let dateNow = Date.now();
-//   let newDateNow = new Date(dateNow);
-//   jewelsArray.forEach(async (item) => {
-//     if (
-//       item.expirationDate.getDate() === newDateNow.getDate() &&
-//       item.expirationDate.getMonth() === newDateNow.getMonth() &&
-//       item.expirationDate.getYear() === newDateNow.getYear() &&
-//       !item.expired
-//     ) {
-//       await Jewel.findByIdAndUpdate(item.id, { expired: true });
-//     }
-//   });
-// };
+const expChecker = async () => {
+  const jewelsArray = await Jewel.find();
+  let jewelsArrayFiltered = jewelsArray.filter((item) => {
+    return item.type.name === "Super" || "VIP";
+  });
+  let dateNow = Date.now();
+  let newDateNow = new Date(dateNow);
+  jewelsArray.forEach(async (item) => {
+    if (
+      item.expirationDate.getDate() === newDateNow.getDate() &&
+      item.expirationDate.getMonth() === newDateNow.getMonth() &&
+      item.expirationDate.getYear() === newDateNow.getYear() &&
+      !item.expired
+    ) {
+      await Jewel.findByIdAndUpdate(item.id, { expired: true });
+    }
+  });
+};
 
-// const subscribtionJob = new CronJob(
-//   "* * * * * *",
-//   expChecker,
-//   null,
-//   false,
-//   "America/Los_Angeles"
-// );
+const subscribtionJob = new CronJob(
+  "* * * * * *",
+  expChecker,
+  null,
+  false,
+  "America/Los_Angeles"
+);
 
-// subscribtionJob.start();
+subscribtionJob.start();
 
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined");
