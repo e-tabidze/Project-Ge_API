ed7d6b4019d1c9acda99294b6a6b8844bc39718f
Elene Tabidze <etabidze@outlook.com>
2021-10-10 18:38:01 +0400

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const stones = require("./routes/stones");
-const metals = require("./routes/metals");
-const pieces = require("./routes/pieces");
-const jewels = require("./routes/jewels");
-const types = require("./routes/types");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
 const config = require("config");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -12,7 +5,19 @@ const express = require("express");
 const app = express();
 const CronJob = require("cron").CronJob;
 const { Jewel } = require("./models/jewel");
+const path = require("path");
+
+// Routes
+const stones = require("./routes/stones");
+const metals = require("./routes/metals");
+const pieces = require("./routes/pieces");
+const jewels = require("./routes/jewels");
+const types = require("./routes/types");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
 const passwordReset = require("./routes/passwordReset");
+
+// const __dirname = path.resolve();
 
 require("dotenv").config();
 
@@ -45,36 +50,60 @@ const subscribtionJob = new CronJob(
 
 subscribtionJob.start();
 
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL ERROR: jwtPrivateKey is not defined");
-  process.exit(1);
-}
-
-// module.exports = function() {
-//   if (!config.has('jwtPrivateKey')) {
-//     throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
-//   }
-// }
+module.exports = function () {
+  if (!config.get("jwtPrivateKey")) {
+    throw new Error("FATAL ERROR: jwtPrivateKey is not defined.");
+  }
+};
 
 mongoose
-  .connect(process.env.DB, {
+  .connect(process.env.MONGODB_URI || process.env.DB, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB..."));
 
-app.use(express.static(`${__dirname}/public`));
+// ** MIDDLEWARE ** //
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:8080",
+  "https://projectge.herokuapp.com",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable");
+      callback(null, true);
+    } else {
+      console.log("Origin rejected");
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
 app.use(cors());
 app.use("/api/stones", stones);
 app.use("/api/metals", metals);
 app.use("/api/pieces", pieces);
 app.use("/api/jewels", jewels);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/types", types);
 app.use("/api/users", users);
 app.use("/api/password-reset", passwordReset);
 app.use("/api/auth", auth);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  // app.use(express.static("client/build"));
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
