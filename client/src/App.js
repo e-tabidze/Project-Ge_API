b9427d25cf9e8174fcc305d410d6b1e56d53f4a9
Elene Tabidze <etabidze@outlook.com>
2021-10-31 16:27:33 +0400

    building

--- client/src/App.js
+++ client/src/App.js
@@ -22,15 +22,14 @@ function App() {
         <ToastContainer />
         <Navbar />
         <Route exact path="/" component={() => <HomePage />} />
-        {/* <Route
+        <Route
           exact
           path="/user-page"
           render={() => {
-            if (!currentUser) return null;
-            return <UserPage />;
+            currentUser && <UserPage />;
           }}
-        /> */}
-        {currentUser && <Route exact path="/" render={() => <UserPage />} />}
+        />
+        {/* {currentUser && <Route exact path="/" render={() => <UserPage />} />} */}
 
         <Route exact path="/product/:id" component={() => <ProductPage />} />
         <Route
