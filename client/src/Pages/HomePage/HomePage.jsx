b749ba41394d39c3d1d43e51911918aae7ace456
Elene Tabidze <etabidze@outlook.com>
2021-10-31 16:56:22 +0400

    building

--- client/src/Pages/HomePage/HomePage.jsx
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -75,6 +75,7 @@ const HomePage = () => {
         </div>
       </div>
       <div className={classes.homepage_ads}>
+        დედისტყვნა
         {/* <img
           src={side}
           alt="ოქროს მარკეტი საქართველოში"
