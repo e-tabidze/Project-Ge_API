72f510256a91dae923eceb091720b050c979b27f
Elene Tabidze <etabidze@outlook.com>
2021-10-31 14:12:11 +0400

    working with DB

--- client/src/Components/ProductModal/ProductModal.jsx
+++ client/src/Components/ProductModal/ProductModal.jsx
@@ -155,7 +155,7 @@ const ProductModal = ({
           )}
         />
 
-        {watchSize === "6127186196d01a09d81632fa" && (
+        {watchSize.piece === "6127186196d01a09d81632fa" && (
           <TextField
             {...register("size")}
             required
@@ -200,7 +200,7 @@ const ProductModal = ({
               <div className={classes.dropdown}>
                 <InputLabel required>სინჯი</InputLabel>
                 <>
-                  {watchMetal.piece === "612695f90dade4d347a56a82" ? (
+                  {watchMetal.metal === "612695f90dade4d347a56a82" ? (
                     <Select fullWidth {...field}>
                       {goldStandards.map((goldStandard) => (
                         <MenuItem
