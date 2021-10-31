fd3d4d13e72337eeb747ee4380cd4c9bbabc32f1
Elene Tabidze <etabidze@outlook.com>
2021-10-31 22:07:01 +0400

    invalid token

--- middleware/auth.js
+++ middleware/auth.js
@@ -13,19 +13,19 @@ function auth(req, res, next) {
   //   res.status(400).send("Invalid token.");
   // }
   try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    const token = req.header("x-auth-token");
+    const decodedToken = jwt.verify(token, "jwtPrivateKey");
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
-      throw 'Invalid user ID';
+      throw "Invalid user ID";
     } else {
       next();
     }
   } catch {
     res.status(401).json({
-      error: new Error('Invalid request!')
+      error: new Error("Invalid request!"),
     });
   }
 }
 
-module.exports = auth; 
+module.exports = auth;
