e2226216fbae964b6fd3751c1ff10658bbfd4e89
Elene Tabidze <etabidze@outlook.com>
2021-10-31 22:01:33 +0400

    invalid token

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,30 @@
 const jwt = require("jsonwebtoken");
-const config = require("config");
+// const config = require("config");
 
 function auth(req, res, next) {
-  const token = req.header("x-auth-token");
-  if (!token) return res.status(401).send("Access denied. No token provided.");
+  // const token = req.header("x-auth-token");
+  // if (!token) return res.status(401).send("Access denied. No token provided.");
 
+  // try {
+  //   const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+  //   req.user = decoded;
+  //   next();
+  // } catch (ex) {
+  //   res.status(400).send("Invalid token.");
+  // }
   try {
-    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
-    req.user = decoded;
-    next();
-  } catch (ex) {
-    res.status(400).send("Invalid token.");
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    const userId = decodedToken.userId;
+    if (req.body.userId && req.body.userId !== userId) {
+      throw 'Invalid user ID';
+    } else {
+      next();
+    }
+  } catch {
+    res.status(401).json({
+      error: new Error('Invalid request!')
+    });
   }
 }
 
