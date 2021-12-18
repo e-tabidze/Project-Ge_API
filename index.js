94748176f747aa6c376be5114e5a10d7f8f0e644
Elene Tabidze <etabidze@outlook.com>
2021-12-19 00:23:11 +0400

    Commited app.use redirectiong

--- index.js
+++ index.js
@@ -97,13 +97,13 @@ app.use("/api/users", users);
 app.use("/api/password-reset", passwordReset);
 app.use("/api/auth", auth);
 
-// if (process.env.NODE_ENV === "production") {
-app.use(express.static(path.join(__dirname, "/client/build")));
-// app.use(express.static("client/build"));
+// // if (process.env.NODE_ENV === "production") {
+// app.use(express.static(path.join(__dirname, "/build")));
+// // app.use(express.static("client/build"));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
-});
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "/build", "index.html"));
+// });
 // }
 
 const port =
